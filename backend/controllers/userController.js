addb7f79d58a10435d9360b95111cb911d155aed
waynnbz <waylonago@gmail.com>
2022-08-08 17:48:19 +0800

    BE invalid user login fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -56,7 +56,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
   // Check for user email
-  const user = await User.findOne(email)
+  const user = await User.findOne({ email })
 
   if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
